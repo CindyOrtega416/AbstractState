7bcd1c6ea6b40d2da207cc32b9a65b28bb621dd2
CindyOrtega416 <cindyortega416@gmail.com>
2019-06-03 05:58:40 -0300

    Codigo de abstract factory listoli

---
+++ Abstract_Factory/cmake-build-debug/CMakeFiles/Abstract_Factory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Abstract_Factory.dir/main.cpp.o"
+  "Abstract_Factory.pdb"
+  "Abstract_Factory"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Abstract_Factory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
